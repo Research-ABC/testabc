6833ff1a8462409095d8010618ca11e2b1e7408a
Chirag Goel <chiraggoel.it@gmail.com>
2024-06-10 13:58:15 +0200

    Update vulnerable.php

--- vulnerable.php
+++ vulnerable.php
@@ -1,4 +1,3 @@
-// The article parameter is assigned to $article variable without any sanitization or validation
-$articleid = $_GET[‘article’];
-// The $articleid parameter is passed as part of the query
-$query = "SELECT * FROM articles WHERE articleid = $articleid";
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
